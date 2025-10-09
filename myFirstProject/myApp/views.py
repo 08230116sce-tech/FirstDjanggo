ff0e85e9574f2177f50d1265eeb069bb1177d921
unknown <08230116.sce@rub.edu.bt>
2025-10-09 13:57:02 +0530

    Adding files

--- myFirstProject/myApp/views.py
+++ myFirstProject/myApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Students
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    students = Students.objects.all()
+    context = {'students': students}
+    return render(request, 'index.html', context)
